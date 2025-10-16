782f638afccaa6ff5a920980766b5958161b1996
Juho Kalberg <Juho.Kalberg@twn.ee>
2025-10-16 10:50:21 +0300

    add github pages workflow

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
   ssr: false,
+  basename: "/testdeploy",
   async prerender() {
     return["/", "/users", "/users/1", "/users/2", "/users/3"]
   }
