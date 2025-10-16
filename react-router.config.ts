81e319b1ab87ea0b7338f728bdb505e520b0833c
Juho Kalberg <Juho.Kalberg@twn.ee>
2025-10-16 11:34:29 +0300

    add github pages workflow

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   appDirectory: "src",
-  ssr: true,
-  basename: process.env.GITHUB_PAGES ? "/testdeploy/" : "/",
+  ssr: !process.env.GITHUB_PAGES,
+  basename: process.env.GITHUB_PAGES ? "/testdeploy" : "/",
   async prerender() {
     return["/", "/users", "/users/1", "/users/2", "/users/3"]
   }
